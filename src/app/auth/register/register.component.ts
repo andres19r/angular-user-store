12e469c70afdc35cb2910683533a0d388139ba2d
Andres Rivero <andres19rivero@gmail.com>
2024-01-17 15:38:42 -0400

    feat(auth): implement register template

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,9 +1,20 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [
+    MatIconModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    RouterModule,
+  ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss',
 })
