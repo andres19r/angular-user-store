6f0894654f8488c741998c51a205dea5073ac370
Andres Rivero <andres19rivero@gmail.com>
2024-01-18 12:47:08 -0400

    feat(auth): implement reactive forms for login & register

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { LoginOptions } from '../../interfaces/auth.interface';
 
 @Component({
-  selector: 'app-login',
+  selector: 'app-l, Validatorsogin',
   standalone: true,
   imports: [
     MatFormFieldModule,
@@ -14,8 +22,25 @@ import { RouterModule } from '@angular/router';
     MatInputModule,
     MatButtonModule,
     RouterModule,
+    ReactiveFormsModule,
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
-export default class LoginComponent {}
+export default class LoginComponent {
+  private authService = inject(AuthService);
+  private router = inject(Router);
+  public loginForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', Validators.required),
+  });
+
+  login() {
+    if (this.loginForm.invalid) return;
+
+    this.authService.login(this.loginForm.value as LoginOptions).subscribe({
+      next: () => this.router.navigate(['product', 'list']),
+      error: (err) => console.error(err),
+    });
+  }
+}
--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,9 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { RegisterOptions } from '../../interfaces/auth.interface';
 
 @Component({
   selector: 'app-register',
@@ -14,8 +22,40 @@ import { RouterModule } from '@angular/router';
     MatFormFieldModule,
     MatButtonModule,
     RouterModule,
+    ReactiveFormsModule,
   ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss',
 })
-export default class RegisterComponent {}
+export default class RegisterComponent {
+  private authService = inject(AuthService);
+  private router = inject(Router);
+  public registerForm = new FormGroup({
+    name: new FormControl('', Validators.required),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(6),
+    ]),
+    confirmPassword: new FormControl('', Validators.required),
+  });
+
+  register() {
+    if (this.registerForm.invalid) return;
+    if (
+      this.registerForm.value.password !==
+      this.registerForm.value.confirmPassword
+    )
+      return;
+
+    const registerOptions: RegisterOptions = {
+      name: this.registerForm.value.name || '',
+      email: this.registerForm.value.email || '',
+      password: this.registerForm.value.password || '',
+    };
+    this.authService.register(registerOptions).subscribe({
+      next: () => this.router.navigate(['product', 'list']),
+      error: (err) => console.error(err),
+    });
+  }
+}
